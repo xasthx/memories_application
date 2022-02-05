6939a5a1566db2237817178d38d1461d6ac80676
Robert Peng <robertpw9@gmail.com>
2022-02-05 00:06:50 -0500

    added frontend components and added logic for get and create post

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import memories from './images/memories.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography cariant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            
+                        </Grid>
+                        <Grid item s={12} sm={4}>
+                            
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
