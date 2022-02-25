63b8775217b3ee046425ffa5520e0b3066a11c77
Robert Peng <robertpw9@gmail.com>
2022-02-25 15:10:00 -0500

    fixed hook

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
-    usEffect(() =>{
+    useEffect(() =>{
         dispatch(getPosts());
     }, [dispatch]);
 
