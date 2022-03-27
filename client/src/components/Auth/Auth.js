e1dd331ab09f1489c06bf677ddcdbf9a9aa70488
Robert Peng <robertpw9@gmail.com>
2022-03-27 16:43:42 -0500

    added google log in authentication and added log in and sign up for front end

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import {signin, signup} from '../../actions/auth';
+
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import useStyles from './styles';
+
+import Input from './Input';
+import Icon from './icon';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
+
+const Auth = () => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setSignup] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    if(isSignup){
+      dispatch(signup(formData, history))
+    }else{
+      dispatch(signin(formData, history))
+
+    }
+  }
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  }
+
+  const switchMode = () => {
+    setSignup((prevSignUp) => !prevSignUp);
+    handleShowPassword(false);
+  }
+
+  const googleSuccess = async (res) => {
+    console.log(res);
+    const result = res?.profileObj;
+    const token = res?.tokenId;
+    try {
+      dispatch({ type: 'AUTH', data: { result, token } });
+
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const googleFailure = (error) => {
+    console.log(error);
+    console.log('Google Sign In failed. Please try again later.');
+  }
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>{isSignup ? 'Sign up' : 'Sign in'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
+                <Input name='lastName' label='Last Name' handleChange={handleChange} half />
+              </>
+            )}
+            <Input name='email' label='Email Address' handleChange={handleChange} type='email' />
+            <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+            {isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' />}
+          </Grid>
+          <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+            {isSignup ? 'Sign Up' : 'Sign In'}
+          </Button>
+          <GoogleLogin
+            clientId='300884562438-97aig38ncca7kbtvkfan4ureabc54c9l.apps.googleusercontent.com'
+            render={(renderProps) => (
+              <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant='contained'>
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy='single_host_origin'
+          />
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  )
+}
+
+export default Auth;
